c213039da8843d80139413d6afb019d36ffbc78e
Antony Kim <111529015+yoggiakjy@users.noreply.github.com>
2024-12-07 11:41:30 +1100

    Update medusa-config.js

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -127,7 +127,17 @@ const medusaConfig = {
       },
     }] : [])
   ],
-  plugins: []
+  plugins: [
+    {
+      resolve: `medusa-payment-paypal`,
+      options: {
+        sandbox: process.env.PAYPAL_SANDBOX,
+        clientId: process.env.PAYPAL_CLIENT_ID,
+        clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+        authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
+      },
+    },
+  ]
 };
 
 console.log(JSON.stringify(medusaConfig, null, 2));
